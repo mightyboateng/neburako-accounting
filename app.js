871d61eb6086139fbe256edef2e338f64ea5de67
Mighty Mike <nanaadams246@gmail.com>
2022-01-06 04:43:36 -0500

    Connect to heroku

--- app.js
+++ app.js
@@ -41,7 +41,10 @@ app.get("/contact", (req, res) => {
   res.render("contact", { message: "" });
 });
 
-const port = 3500;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 app.listen(port, function () {
   console.log("Server started on port " + port + " ............");
 });
--- components/database.js
+++ components/database.js
@@ -6,8 +6,9 @@ const connection = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
+  port: 3306,
   database: process.env.DATABASE_DATABASE,
-  // ssl: { ca: fs.readFileSync(__dirname + "\\scale_ssl.txt") },
+  ssl: { ca: fs.readFileSync(__dirname + "/scale_ssl.txt") },
 });
 
 // ######### Connect to Database ########
